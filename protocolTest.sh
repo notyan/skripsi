2064d85aae6cd3124d81f845278767f1c99fb234
notyan <mdevaroyan@gmail.com>
2024-10-10 15:53:42 +0700

    Change conditional to Case

--- protocolTest.sh
+++ protocolTest.sh
@@ -5,15 +5,19 @@ for alg in "${algorithm[@]}"
 do
     for i in {1..3}
     do
-        if [[ "$alg" == "dilithium" ]]: then
-            python long-term.py $1 -o keys/cl_dilithium -pq;
-            python client.py $1 -test -pq
-        elif [[ "$alg" == "rsa" ]]: then
-            python long-term.py $1 -o keys/cl_rsa -rsa;
-            python client.py $1 -test -rsa;
-        else
-            python long-term.py $1 -o keys/cl_ecdsa;
-            python client.py $1 -test;
-        fi
+        case "$alg" in
+            "dilithium")
+                python long-term.py $1 -o keys/cl_dilithium -pq;
+                python client.py $1 -test -pq
+                ;;
+            "rsa")
+                python long-term.py $1 -o keys/cl_rsa -rsa;
+                python client.py $1 -test -rsa
+                ;;
+            *)
+                python long-term.py $1 -o keys/cl_ecdsa;
+                python client.py $1 -test
+                ;;
+        esac
     done
 done
