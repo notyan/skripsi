8c80b3ac7d3d876f64998b30bff8f65e1ab58b7c
notyan <mdevaroyan@gmail.com>
2024-10-13 15:01:33 +0700

    enable all algoirthm

--- serverKeygen.sh
+++ serverKeygen.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 #Generate Keypair 
 algorithm=("dil" "ecdsa" "rsa")
-#algorithm=("dil")
+#algorithm=("dil" "ecdsa")
 #Test For normal condition
 mkdir -p "keys/server"
 mkdir -p "keys/client"
 
 for alg in "${algorithm[@]}"
 do
-    keys="keys/server/$alg"
     for i in {1..3}
+    keys="keys/server/$alg$i"
     do
         case "$alg" in
             "dil")
-                python long-term.py $i -o $keys$i -pq --silent 
+                python long-term.py $i -o $keys -pq --silent 
                 ;;
             "rsa")
-                python long-term.py $i -o $keys$i -rsa --silent
+                python long-term.py $i -o $keys -rsa --silent
                 ;;
             *)
-                python long-term.py $i -o $keys$i --silent
+                python long-term.py $i -o $keys --silent
                 ;;
         esac
     done
