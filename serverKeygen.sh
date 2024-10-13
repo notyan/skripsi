ca886b240c8b07afd36e090c323e909610ea11ff
notyan <mdevaroyan@gmail.com>
2024-10-13 14:57:12 +0700

    Add server key generation script

---
+++ serverKeygen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#Generate Keypair 
+algorithm=("dil" "ecdsa" "rsa")
+#algorithm=("dil")
+#Test For normal condition
+mkdir -p "keys/server"
+mkdir -p "keys/client"
+
+for alg in "${algorithm[@]}"
+do
+    keys="keys/server/$alg"
+    for i in {1..3}
+    do
+        case "$alg" in
+            "dil")
+                python long-term.py $i -o $keys$i -pq --silent 
+                ;;
+            "rsa")
+                python long-term.py $i -o $keys$i -rsa --silent
+                ;;
+            *)
+                python long-term.py $i -o $keys$i --silent
+                ;;
+        esac
+    done
+done
